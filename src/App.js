415f54ab3e3b22f27d5691ba93d5d152706d0675
paulsaitabau <paulsaitabau05@gmail.com>
2024-05-16 19:02:42 +0300

    complete lab

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
 function App() {
-  return <div>Your code here</div>;
+  return(
+  <main>
+  <h1>Hi, I'm Paul Saitabau</h1>
+  <img alt="My profile pic" src="https://via.placeholder.com/350" />
+  <h2>About Me</h2>
+  <p>
+    Personal Information:<br />
+    - Name: Paul Saitabau<br />
+    - Date of Birth: April 8, 2005<br />
+    - Residence: Nairobi, Kasarani<br /><br />
+
+    Education:<br />
+    - Current Student: Software Engineering, Moringa School (Phase 1)<br /><br />
+
+    Interests and Achievements:<br />
+    - Technology: Passionate about exploring and learning new technologies.<br />
+    - Photography: Enjoys capturing moments and landscapes through the lens.<br />
+    - Hiking: Completed several long-distance hikes as part of the Presidential Award program. Gold Award Holder.<br /><br />
+
+    Spiritual Attributes:<br />
+    - Courageous: Demonstrates bravery and resilience in various aspects of life.<br />
+    - Leadership Skills: Exhibits strong leadership qualities, contributing positively to teams and communities.
+</p>
+
+  <div>
+    <a href="https://github.com">GitHub</a>
+    <a href="https://linkedin.com">LinkedIn</a>
+  </div>
+</main>
+);
 }
+
 
 export default App;
